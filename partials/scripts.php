e1d1feede1e8f9a5206376d86fb757bc38798d5c
mifada2543 <minecraft.daffa2501@gmail.com>
2026-08-02 18:46:19 +0700

    add cache-busting

--- partials/scripts.php
+++ partials/scripts.php
@@ -24,4 +24,4 @@ $scripts_root = $scripts_root ?? '';
 ?>
 <script src="<?= $scripts_root ?>assets/js/compatibilitas/sweetalert2.all.min.js"></script>
 <script src="<?= $scripts_root ?>assets/js/compatibilitas/script.min.js"></script>
-<script src="<?= $scripts_root ?>assets/js/shared/health-reminder.js"></script>
+<script src="<?= $scripts_root ?>assets/js/shared/health-reminder.js?v=<?= filemtime(__DIR__ . '/../assets/js/shared/health-reminder.js') ?>"></script>
